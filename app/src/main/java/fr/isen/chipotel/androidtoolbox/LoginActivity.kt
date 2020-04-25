6db0c35fc53f4c1fc2940e19c4926121c723296d
maximechipotel <maxime.chipotel@orange.fr>
2020-04-25 20:03:52 +0800

    interface

--- app/src/main/java/fr/isen/chipotel/androidtoolbox/LoginActivity.kt
+++ app/src/main/java/fr/isen/chipotel/androidtoolbox/LoginActivity.kt
@@ -3,10 +3,17 @@ package fr.isen.chipotel.androidtoolbox
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_login.*
+
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        button_valider.setOnClickListener() {
+            Toast.makeText(applicationContext,R.string.toast_valider,Toast.LENGTH_LONG).show()
+        }
     }
 }
