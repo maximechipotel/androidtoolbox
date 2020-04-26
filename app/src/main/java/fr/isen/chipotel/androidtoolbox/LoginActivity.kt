f2516838e9845bb58c9f9ba45cdc0361753a01ff
maximechipotel <maxime.chipotel@orange.fr>
2020-04-26 12:57:58 +0800

    End part II

--- app/src/main/java/fr/isen/chipotel/androidtoolbox/LoginActivity.kt
+++ app/src/main/java/fr/isen/chipotel/androidtoolbox/LoginActivity.kt
@@ -1,5 +1,6 @@
 package fr.isen.chipotel.androidtoolbox
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -12,8 +13,22 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        button_valider.setOnClickListener() {
-            Toast.makeText(applicationContext,R.string.toast_valider,Toast.LENGTH_LONG).show()
+        button_valider.setOnClickListener {
+           // var status=if(email_et.text.toString().equals("admin")
+           //     && password_et.text.toString().equals("123")) "Bienvenu" else "Mot de passe ou nom d'utilisateur incorrect"
+            //Toast.makeText(this,status,Toast.LENGTH_LONG).show()
+
+            if(email_et.text.toString().equals("admin")
+                && password_et.text.toString().equals("123"))
+            {
+                val intent = Intent(this, HomeActivity::class.java)
+                startActivity(intent)
+                Toast.makeText(this,"Bienvenue",Toast.LENGTH_LONG).show()
+            }
+
+            else
+                Toast.makeText(this,"Mot de passe ou email incorrect",Toast.LENGTH_LONG).show()
+
         }
     }
 }
