41f6298ec53f0fb40735a591501b4140611efa0c
maximechipotel <maxime.chipotel@orange.fr>
2020-04-30 16:57:19 +0800

    Fragment/cycleDeVie

---
+++ app/src/main/java/fr/isen/chipotel/androidtoolbox/CycleActivity.kt
@@ -0,0 +1,89 @@
+package fr.isen.chipotel.androidtoolbox
+
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+
+
+class CycleActivity : AppCompatActivity() {
+
+    var isFragmentOneLoaded = true;
+    val manager = supportFragmentManager
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cycle)
+        //print("OnCreate")
+        //Toast.makeText(this,"onCreate",Toast.LENGTH_SHORT).show()
+
+        val Change = findViewById<Button>(R.id.button_fragment)
+        ShowFragmentOne()
+        Change.setOnClickListener(({
+            if(isFragmentOneLoaded)
+                ShowFragmentTwo()
+            else
+                ShowFragmentOne()}))
+
+    }
+
+    fun ShowFragmentOne(){
+        val transaction = manager.beginTransaction()
+        val fragment = FragmentOne()
+        transaction.replace(R.id.fragment_holder,fragment)
+        transaction.addToBackStack(null)
+        transaction.commit()
+        isFragmentOneLoaded = true
+    }
+    fun ShowFragmentTwo() {
+        val transaction = manager.beginTransaction()
+        val fragment = FragmentTwo()
+        transaction.replace(R.id.fragment_holder, fragment)
+        transaction.addToBackStack(null)
+        transaction.commit()
+        isFragmentOneLoaded = false
+    }
+}
+
+
+   /* override fun onStart() {
+        super.onStart()
+        print("OnStart")
+        Toast.makeText(this,"onStart", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        print("onResume")
+        Toast.makeText(this,"onResume",Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        print("onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        print("onStop")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        print("onRestart")
+        Toast.makeText(this,"onRestart",Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        print("Destroy")
+    }
+    fun print(msg: String){
+        Log.d("Activity state",msg)
+
+    }
+*/
+
--- app/src/main/java/fr/isen/chipotel/androidtoolbox/HomeActivity.kt
+++ app/src/main/java/fr/isen/chipotel/androidtoolbox/HomeActivity.kt
@@ -1,12 +1,20 @@
 package fr.isen.chipotel.androidtoolbox
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_home.*
 
 class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+
+        button_cycle.setOnClickListener() {
+            val intent = Intent(this, CycleActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
